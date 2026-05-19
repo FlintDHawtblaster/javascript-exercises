8e1585cfe0e43a7b72413bf47ea19df349cd1043
FlintJunior <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-05-19 13:28:23 +0000

    Almost done with the exercises

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    reversedWord = "";
+    for (let i = word.length-1; i > -1; i--) {
+        reversedWord += word[i];
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+console.log(reverseString("hello there"));
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(anArray, ...thisArgs) {
+    for (let i = 0; i < anArray.length; i++) {
+        for (let j = 0; j < thisArgs.length; j++) {
+            if (thisArgs[j] === anArray[i]) {
+                anArray.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    return anArray;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//console.log(removeFromArray([1, 2, 3, 4], 3));
