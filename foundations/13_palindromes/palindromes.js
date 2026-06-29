33a29c52d041d28534887960af30049821dd67c6
FlintJunior <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-06-29 12:48:48 +0000

    Completed exercises 12 to 14 in the foundations folder

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let noPunctuation = text.replace(/[^\w]|_/g, '').toLowerCase();
+    const temp = [...noPunctuation];
+    let reverseText = temp.reverse().join("");
+    
+    return reverseText === noPunctuation;
 };
 
 // Do not edit below this line
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let num = parseInt(a, 10);
 
+    if (num > 0) {
+        let newArray = [1, 1]
+        for (let i = 2; i < num; i++) {
+            nextValue = newArray[i-1] + newArray[i-2];
+            newArray.push(nextValue);
+        }
+
+        return newArray[num-1];
+    } else if (num === 0) {
+        return num;
+    } else {
+        return "OOPS";
+    }
+    
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+console.log(parseInt("0", 10));
